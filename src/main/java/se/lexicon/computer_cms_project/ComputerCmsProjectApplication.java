1479363b3e0a024b20b57241ce57c1e75d113ba0
jon0945 <john.gullne@gmail.com>
2020-09-07 18:21:15 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/computer_cms_project/ComputerCmsProjectApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.computer_cms_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerCmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerCmsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/computer_cms_project/ComputerCmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.computer_cms_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerCmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
